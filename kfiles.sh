138a06e733ecb4e96ee7bf0280cff37034ff317f
Jianwu <jianwu.hu@intel.com>
2013-06-13 06:26:15 +0800

    update kfile.sh

--- kfiles.sh
+++ kfiles.sh
@@ -1,23 +1,30 @@
 #! /bin/bash
+if [ $ANDROID_BUILD_TOP ]; then
+   echo "Android built!"
+else
+    echo "Build Env not setup!!"
+    exit 1
+fi
 cd $ANDROID_BUILD_TOP/kernel/include
 all_includes=`find . -type d |grep -v xen`
 cd $ANDROID_PRODUCT_OUT/kernel_build
 all_objects=`find .  -type f -name \*.o`
 for obj in $all_objects
     do
+    obj=${obj#./}
     name=${obj%.o}
     file=${name##./*/}
     c_src=$name.c
     s_src=$name.S
     c_hdr=$name.h 
     if [ -f $ANDROID_BUILD_TOP/kernel/$c_src ]; then
-    echo $c_src
+        echo $ANDROID_BUILD_TOP/kernel/$c_src
     fi
     if [ -f $ANDROID_BUILD_TOP/kernel/$s_src ]; then
-    echo $s_src
+        echo $ANDROID_BUILD_TOP/kernel/$s_src
     fi
     if [ -f $ANDROID_BUILD_TOP/kernel/$c_hdr ]; then
-    echo $c_hdr
+        echo $ANDROID_BUILD_TOP/kernel/$c_hdr
     fi
     for include in $all_includes
         do
