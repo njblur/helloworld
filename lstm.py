2b058912b44b8a9d5d93a191005af3684a35440a
Jianwu <njblur@gmail.com>
2016-11-22 07:53:02 +0800

    add dropout

--- lstm.py
+++ lstm.py
@@ -12,14 +12,16 @@ batch_size = 40
 hidden_size = 100
 
 lstm_cell = tf.nn.rnn_cell.LSTMCell(hidden_size)
-lstm_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell]*2)
-initial_state = lstm_cell.zero_state(batch_size, tf.float32)
+lstm_drop = tf.nn.rnn_cell.DropoutWrapper(lstm_cell,output_keep_prob=1.0)
+initial_state = lstm_drop.zero_state(batch_size, tf.float32)
 test_state = lstm_cell.zero_state(1, tf.float32)
 inputs = tf.placeholder(dtype = tf.float32,shape=[None,1])
 labels = tf.placeholder(dtype = tf.float32,shape=[None,1])
 
+inputs_drop = tf.nn.dropout(inputs,1.0)
+
 state = initial_state
-(cell_output, state) = lstm_cell(inputs, state)
+(cell_output, state) = lstm_drop(inputs_drop, state)
 
 weights = tf.Variable(tf.zeros([hidden_size,1]))
 bias = tf.Variable(3.0)
@@ -42,7 +44,7 @@ loss = tf.reduce_sum(loss,1)
 
 loss = tf.reduce_mean(loss)
 
-min = tf.train.AdadeltaOptimizer(2.0).minimize(loss)
+min = tf.train.AdadeltaOptimizer(1.0).minimize(loss)
 
 with tf.Session() as sess:
     tf.initialize_all_variables().run()
@@ -53,9 +55,15 @@ with tf.Session() as sess:
         r,m,istate = sess.run([loss,min,state],feed_dict={inputs:_inputs[i*batch_size:(i+1)*batch_size,:],labels:_labels[i*batch_size:(i+1)*batch_size,:],initial_state:istate})
     print(r)
     istate = sess.run(test_state)
-    print(istate)
-    test = [1,1,2,1,3,1,1,1,2,1,0,1,1,1,2,3,0,1,3]
-    print(test)
-    for t in test:
+
+    # test = [1,1,2,1,3,1,1,0,2,1,0,1,1,0,2,3,0,1,3]
+    test = np.loadtxt("_inputs")
+    target = np.loadtxt("_labels")
+    data = test[0:20]
+    t = target[0:20]
+    print(data)
+    print(t)
+
+    for t in data:
         r,istate = sess.run([tactive,tstate],feed_dict={inputs:[[t]],test_state:istate})
         print(r)
